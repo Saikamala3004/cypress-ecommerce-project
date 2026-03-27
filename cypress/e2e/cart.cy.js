32c6a7b9b7f09f121fb15e7a5a3173bd389ded63
Saikamala <saikamalac@gmail.com>
2026-03-27 19:27:48 +0530

    Added cart functionality tests (add/remove product)

---
+++ cypress/e2e/cart.cy.js
@@ -0,0 +1,38 @@
+describe('SauceDemo Cart Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+
+    // Login first
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+  })
+
+  it('User should add product to cart successfully', () => {
+
+    // Add product
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+    // Go to cart
+    cy.get('.shopping_cart_link').click()
+
+    // Verify product is added
+    cy.contains('Sauce Labs Backpack').should('be.visible')
+
+  })
+
+  it('User should remove product from cart', () => {
+
+  cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
+  cy.get('.shopping_cart_link').click()
+
+  // Remove item
+  cy.get('[data-test="remove-sauce-labs-bike-light"]').click()
+
+  // Verify cart is empty
+  cy.get('.cart_item').should('not.exist')
+
+})
+
+})
