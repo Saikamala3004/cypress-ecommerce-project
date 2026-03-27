3401a68b26926adb5976f7a17d26371286fb7d60
Saikamala <saikamalac@gmail.com>
2026-03-27 19:21:44 +0530

    Added valid and invalid login test with beforeEach

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,23 @@
-describe('SauceDemo Login Test', () => {
+describe('SauceDemo Login Tests', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
 
   it('Valid Login', () => {
-    cy.visit('https://www.saucedemo.com/')
-
     cy.get('#user-name').type('standard_user')
     cy.get('#password').type('secret_sauce')
     cy.get('#login-button').click()
 
     cy.url().should('include', 'inventory')
+  })
+
+  it('Invalid Login', () => {
+    cy.get('#user-name').type('locked_out_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+    cy.get('[data-test="error"]').should('be.visible')
   })
 
 })
